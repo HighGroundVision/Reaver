0dff7acd5b74ac5270d523822bcb5ba232e02f5a
RGBKnights <master@rgbknights.com>
2021-10-17 18:40:51 -0400

    updated to remove the section

--- src/HGV.Reaver/Startup.cs
+++ src/HGV.Reaver/Startup.cs
@@ -22,10 +22,10 @@ namespace HGV.Reaver
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            var section = Configuration.GetSection("Settings");
-            var settings = section.Get<ReaverSettings>();
-            services.Configure<ReaverSettings>(section);
+            var settings = Configuration.Get<ReaverSettings>();
 
+            services.Configure<ReaverSettings>(Configuration);
+            
             services.AddAuthentication(options =>
             {
                 options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
